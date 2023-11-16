009714fedc9979262ed2d4c931a0ec7b72fe0c4c
Athar M <93383101+KingPin-1@users.noreply.github.com>
2023-11-17 04:22:25 +0530

    Complete Project. main.ts populated with ListTemplate model structure and methods.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,35 @@
 import './css/style.css';
+import FullList from './models/FullList';
+import ListItem from './models/ListItem';
+import ListTemplate from './templates/ListTemplate';
+
+const initApp = (): void => {
+    const fullList = FullList.instance;
+    const template = ListTemplate.instance;
+
+    const itemEntryForm = document.getElementById('itemEntryForm') as HTMLFormElement;
+    itemEntryForm.addEventListener('submit', (event: SubmitEvent) => {
+        event.preventDefault();
+
+        const input = document.getElementById('newItem') as HTMLInputElement;
+        const newEntryText: string = input.value.trim();
+        if (!newEntryText.length) return;
+
+        const itemId: number = fullList.list.length ? parseInt(fullList.list[fullList.list.length - 1].id) + 1 : 1;
+        const newItem = new ListItem(itemId.toString(), newEntryText);
+        fullList.addItem(newItem);
+        template.render(fullList);
+        input.value = '';
+    });
+
+    const clearItemsButton = document.getElementById('clearItemsButton') as HTMLButtonElement;
+    clearItemsButton.addEventListener('click', () => {
+        fullList.clearList();
+        template.clear();
+    });
+
+    fullList.load();
+    template.render(fullList);
+};
+
+document.addEventListener('DOMContentLoaded', initApp);
---
+++ src/templates/ListTemplate.ts
@@ -0,0 +1,59 @@
+import FullList from '../models/FullList';
+
+interface DOMList {
+    ul: HTMLUListElement;
+    clear(): void;
+    render(fullList: FullList): void;
+}
+
+export default class ListTemplate implements DOMList {
+    ul: HTMLUListElement;
+    static instance: ListTemplate = new ListTemplate();
+
+    private constructor() {
+        this.ul = document.getElementById('listItems') as HTMLUListElement;
+    }
+
+    clear(): void {
+        this.ul.innerHTML = ``;
+    }
+
+    render(fullList: FullList): void {
+        this.clear();
+        
+        fullList.list.forEach((listItem) => {
+            const listItemNode = document.createElement('li') as HTMLLIElement;
+            listItemNode.className = 'item';
+
+            const check = document.createElement('input') as HTMLInputElement;
+            check.type = 'checkbox';
+            check.checked = listItem.checked;
+            check.tabIndex = 0;
+            check.id = listItem.id;
+
+            check.addEventListener('change', () => {
+                listItem.checked = !listItem.checked;
+                fullList.save();
+            });
+
+            const label = document.createElement('label') as HTMLLabelElement;
+            label.htmlFor = listItem.id;
+            label.innerText = listItem.item;
+
+            const button = document.createElement(
+                'button'
+            ) as HTMLButtonElement;
+            button.className = 'button';
+            button.innerText = 'X';
+
+            button.addEventListener('click', () => {
+                fullList.removeItem(listItem.id);
+            });
+
+            listItemNode.appendChild(check);
+            listItemNode.appendChild(label);
+            listItemNode.appendChild(button);
+            this.ul.appendChild(listItemNode);
+        });
+    }
+}
