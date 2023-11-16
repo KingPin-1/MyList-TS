07ee94d373443840df474e2b06c45e78d4810d8c
Athar M <93383101+KingPin-1@users.noreply.github.com>
2023-11-17 03:20:54 +0530

    Singleton Model of List Added

--- src/models/ListItem.ts
+++ src/models/ListItem.ts
@@ -6,26 +6,26 @@ export interface Item {
 
 export default class ListItem implements Item {
     constructor(
-        private _id: string,
-        private _item: string,
-        private _checked: boolean
+        private _id: string = '',
+        private _item: string = '',
+        private _checked: boolean = false
     ) {}
     get id(): string {
         return this._id;
     }
-    set id(val: string) {
-        this._id = val;
+    set id(id: string) {
+        this._id = id;
     }
     get item(): string {
         return this._item;
     }
-    set item(val: string) {
-        this._item = val;
+    set item(item: string) {
+        this._item = item;
     }
     get checked(): boolean {
         return this._checked;
     }
-    set checked(val: boolean) {
-        this._checked = val;
+    set checked(checked: boolean) {
+        this._checked = checked;
     }
 }
